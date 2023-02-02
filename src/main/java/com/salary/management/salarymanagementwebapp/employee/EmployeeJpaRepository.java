2f1e2e3fb531157d8fccb2fdb863398b09f34511
Ong-Jun-Kai <ongjunkai@ecquaria.com>
2023-02-02 18:25:28 +0800

    getAllEmployees by request param and salary range

--- src/main/java/com/salary/management/salarymanagementwebapp/employee/EmployeeJpaRepository.java
+++ src/main/java/com/salary/management/salarymanagementwebapp/employee/EmployeeJpaRepository.java
@@ -1,8 +1,10 @@
 package com.salary.management.salarymanagementwebapp.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Repository
@@ -10,4 +12,7 @@ public interface EmployeeJpaRepository extends JpaRepository<Employee, String>{
 //	List<Employee> findByUsername(String login);
 
 	List<Employee> findByLogin(String login);
+
+	@Query("SELECT u FROM Employee u WHERE u.salary >= ?1 and u.salary <= ?2")
+	List<Employee> findEmployeeBySalary(BigDecimal minSalary, BigDecimal maxSalary);
 }
--- src/main/java/com/salary/management/salarymanagementwebapp/employee/EmployeeJpaResource.java
+++ src/main/java/com/salary/management/salarymanagementwebapp/employee/EmployeeJpaResource.java
@@ -1,13 +1,18 @@
 package com.salary.management.salarymanagementwebapp.employee;
 
+import com.salary.management.salarymanagementwebapp.message.ResponseMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.math.BigDecimal;
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
@@ -18,9 +23,28 @@ public class EmployeeJpaResource {
 	private EmployeeJpaRepository employeeJpaRepository;
 
 	
-	@GetMapping("/jpa/users/employees")
+	/*@GetMapping("/jpa/users/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeJpaRepository.findAll();
+	}*/
+
+	@GetMapping("/jpa/users/employees")
+	@ResponseBody
+	public ResponseEntity<Object> getAllEmployees
+			(@RequestParam(defaultValue = "0") BigDecimal minSalary,
+			 @RequestParam(defaultValue = "99999999") BigDecimal maxSalary,
+			 @RequestParam(defaultValue = "0") BigDecimal offset,
+			 @RequestParam(defaultValue = "30") BigDecimal limit,
+			 @RequestParam(defaultValue = "asc") String sort){
+		Map<String, Object> responseMap = new HashMap<>();
+		try {
+			responseMap.put("results", employeeJpaRepository.findEmployeeBySalary(minSalary, maxSalary));
+		} catch (Exception e) {
+			throw new ResponseStatusException(
+					HttpStatus.BAD_REQUEST);
+		}
+//		return "minSalary: " + minSalary + " maxSalary: " + maxSalary + " offset: " + offset + " limit: " + limit + " sort: " + sort;
+		return new ResponseEntity<>(responseMap, HttpStatus.OK);
 	}
 
 	@GetMapping("/jpa/users/employees/{id}")
